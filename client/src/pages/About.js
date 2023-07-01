287086c96c02ae64c29e9fa80f8c5f1cdd08e203
Brit Sovic <bsovic@tulane.edu>
2023-07-01 17:31:28 -0400

    home page setup

---
+++ client/src/pages/About.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function About() {
+    return(
+        <section>
+            <div class="container">
+                <div class="m-5 row">
+                    <div class="col">
+                        <h1>About Brit</h1>
+                    </div>
+                    <div class="col">
+                        <h1>About Application</h1>
+                    </div>
+                    <div class="col">
+                        <h1>Application Updates</h1>
+                        <p>
+                            The application will be undergoing continuous updates in order to improve functionaility
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default About;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const Home = () => {
-    <section class='home'>
-        HOME
+    return(
+        <section class='home'>
+        <header>
+            NAVBAR
+        </header>
+        <section class='home-navbar'>
+            Welcome to the duplicate check system
+        </section>
+        <footer class='footer'>
+            FOOTER
+        </footer>
     </section>
+    )
 };
 
 export default Home;
